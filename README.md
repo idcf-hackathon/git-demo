a72130da3b1d35b2fd8a26d005eb6c3f60686d1a
Lei Xu - Azure DevOps MVP <leixu@leansoftx.com>
2019-11-20 20:55:41 +0800

    C7 on hotfix

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # IDCF 训练营 - Git演示程序
 
 本演示程序用于展示Git库的基本功能。
+
+C7 on hotfix
--- src/aspnet-mvc/Controllers/HomeController.cs
+++ src/aspnet-mvc/Controllers/HomeController.cs
@@ -11,6 +11,7 @@ namespace aspnet_mvc.Controllers
 {
     public class HomeController : Controller
     {
+        // C7 on hotfix
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
